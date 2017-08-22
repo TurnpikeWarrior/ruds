c63f1354375830c71eae0404953ed05c1b40dd3d
Andy Oh <andyoh2@gmail.com>
2017-08-22 00:25:31 -0400

    latest update to PyBank h/w

--- 03-Python/PyBank/main.py
+++ 03-Python/PyBank/main.py
@@ -5,13 +5,52 @@
 
 import os
 import csv
+import operator
+
+# files = ['budget_data_1.csv', 'budget_data_2']
+
+csvpath = os.path.join('Resources', 'budget_data_1.csv')
+
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+    count_row = 0
+    total_revenue = 0
+    max_rev = 0
+
+    budget_chart = []
+
+    for row in csvreader:
+        # date = row[0]
+        # revenue = float(row[1])
+        count_row += 1
+        total_revenue += int(row[1])
+        # budget_chart.append({"Date": date, "Revenue": revenue})
+
+    avg_revenue = float(total_revenue / count_row)
+    # max_rev = max(budget_chart[Revenue])
+
+    print(total_revenue)
+    print(max_rev)
+
+    print("Financial Analysis")
+    print("----------------------------")
+    print("Total Months: " + str(count_row))
+    print("Total Revenue: $" + str(total_revenue))
+    print("Average Revenue Change: $" + str(avg_revenue))
+    # print("Greatest Increase in Revenue: " + max_rev)
+    # print("Greatest Decrease in Revenue: " + min_rev)
 
 
+    # Specify the file to write to
+    with open('result_PyBank.csv', 'w', newline='') as csvfile:
 
-print("Financial Analysis")
-print("----------------------------")
-print("Total Months: ")
-print("Total Revenue: $")
-print("Average Revenue Change: $")
-print("Greatest Increase in Revenue: ")
-print("Greatest Decrease in Revenue: ")
+        # Initialize csv.writer
+        csvwriter = csv.writer(csvfile, delimiter=',')
+
+        # Write the first row (column headers)
+        csvwriter.writerow(['Financial Analysis'])
+        csvwriter.writerow([''])
+        csvwriter.writerow(['Total Months', str(count_row)])
+        csvwriter.writerow(['Total Revenue ($)', str(total_revenue)])
+        csvwriter.writerow(['Average Revenue Change ($)', str(avg_revenue)])
